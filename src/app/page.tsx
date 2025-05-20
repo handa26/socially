de4eab01a0d8470f2a24e23b3d6caebd5a18e758
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-05-20 09:22:53 +0800

    feat: Add button component and update styles with Tailwind CSS integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,15 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <SignedOut>
-        <SignInButton mode="modal" />
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
         <SignUpButton />
       </SignedOut>
       <SignedIn>
