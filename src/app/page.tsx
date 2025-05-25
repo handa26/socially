bab252c334a22249628240e9207f487094612674
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-05-25 16:20:10 +0800

    feat: Add FollowButton and WhoToFollow components for user interaction and recommendations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
 import { currentUser } from "@clerk/nextjs/server";
 
 import CreatePost from "@/components/CreatePost";
+import WhoToFollow from "@/components/WhoToFollow";
 
 export default async function Home() {
   const user = await currentUser();
 
   return (
     <div className="grid grid-cols-1 gap-6 lg:grid-cols-10">
-      <div className="lg:col-span-6">
-        {user ? <CreatePost /> : null}
-      </div>
+      <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
 
       <div className="hidden sticky top-20 lg:block lg:col-span-4">
-        WhoToFollow
+        <WhoToFollow />
       </div>
     </div>
   );
