a7d17331ad1eaacce794317a3c2c2466978b1be7
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-05-27 16:17:31 +0800

    feat: Add PostCard and DeleteAlertDialog components; update package dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,27 @@ import { currentUser } from "@clerk/nextjs/server";
 
 import CreatePost from "@/components/CreatePost";
 import WhoToFollow from "@/components/WhoToFollow";
+import PostCard from "@/components/PostCard";
+
+import { getPosts } from "@/actions/post.action";
+import { getDbUserId } from "@/actions/user.action";
 
 export default async function Home() {
   const user = await currentUser();
+  const posts = await getPosts();
+  const dbUserId = await getDbUserId();
 
   return (
     <div className="grid grid-cols-1 gap-6 lg:grid-cols-10">
-      <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
+
+        <div className="space-y-6">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+          ))}
+        </div>
+      </div>
 
       <div className="hidden sticky top-20 lg:block lg:col-span-4">
         <WhoToFollow />
---
+++ src/components/PostCard.tsx
@@ -0,0 +1,138 @@
+"use client";
+import { useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import toast from "react-hot-toast";
+import Link from "next/link";
+import { formatDistanceToNow } from "date-fns";
+
+import { Card, CardContent } from "./ui/card";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import DeleteAlertDialog from "./DeleteAlertDialog";
+
+import {
+  getPosts,
+  toggleLike,
+  createComment,
+  deletePost,
+} from "@/actions/post.action";
+
+type Posts = Awaited<ReturnType<typeof getPosts>>;
+type Post = Posts[number];
+
+const PostCard = ({
+  post,
+  dbUserId,
+}: {
+  post: Post;
+  dbUserId: string | null;
+}) => {
+  const { user } = useUser();
+
+  const [newComment, setNewComment] = useState("");
+  const [isCommenting, setIsCommenting] = useState(false);
+  const [isLiking, setIsLiking] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [hasLiked, setHasLiked] = useState(
+    post.likes.some((like) => like.userId === dbUserId)
+  );
+  const [optimisticLikes, setOptimisticLikes] = useState(post._count.likes);
+
+  const handleLike = async () => {
+    if (isLiking) return;
+
+    try {
+      setIsLiking(true);
+      setHasLiked((prevState) => !prevState);
+      setOptimisticLikes((prevState) => prevState + (hasLiked ? -1 : 1));
+      await toggleLike(post.id);
+    } catch (error) {
+      setOptimisticLikes(post._count.likes);
+      setHasLiked(post.likes.some((like) => like.userId === dbUserId));
+    } finally {
+      setIsLiking(false);
+    }
+  };
+
+  const handleComment = async () => {
+    if (!newComment.trim() || isCommenting) return;
+
+    try {
+      setIsCommenting(true);
+      const result = await createComment(post.id, newComment);
+      if (result?.success) {
+        toast.success("Comment posted successfully");
+        setNewComment("");
+      }
+    } catch (error) {
+      toast.error("Failed to add comment");
+    } finally {
+      setIsCommenting(false);
+    }
+  };
+
+  const handleDeletePost = async () => {
+    if (isDeleting) return;
+
+    try {
+      setIsDeleting(true);
+      const result = await deletePost(post.id);
+      if (result.success) toast.success("Post deleted successfully");
+      else throw new Error(result.error);
+    } catch (error) {
+      toast.error("Failed to delete post");
+    } finally {
+      setIsDeleting(false);
+    }
+  };
+
+  return (
+    <Card className="overflow-hidden">
+      <CardContent className="p-4 sm:p-6">
+        <div className="space-y-4">
+          <div className="flex space-x-3 sm:space-x-4">
+            <Link href={`/profile/${post.author.username}`}>
+              <Avatar className="size-8 sm:w-10 sm:h-10">
+                <AvatarImage src={post.author.image ?? "/avatar.png"} />
+              </Avatar>
+            </Link>
+
+            {/* POST HEADER & TEXT CONTENT */}
+            <div className="flex-1 min-w-0">
+              <div className="flex items-start justify-between">
+                <div className="flex flex-col sm:flex-row sm:items-center sm:space-x-2 truncate">
+                  <Link
+                    href={`/profile/${post.author.username}`}
+                    className="font-semibold truncate"
+                  >
+                    {post.author.name}
+                  </Link>
+                  <div className="flex items-center space-x-2 text-sm text-muted-foreground">
+                    <Link href={`/profile/${post.author.username}`}>
+                      @{post.author.username}
+                    </Link>
+                    <span>•</span>
+                    <span>
+                      {formatDistanceToNow(new Date(post.createdAt))} ago
+                    </span>
+                  </div>
+                </div>
+                {/* Check if current user is the post author */}
+                {dbUserId === post.author.id && (
+                  <DeleteAlertDialog
+                    isDeleting={isDeleting}
+                    onDelete={handleDeletePost}
+                  />
+                )}
+              </div>
+              <p className="mt-2 text-sm text-foreground break-words">
+                {post.content}
+              </p>
+            </div>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default PostCard;
