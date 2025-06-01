7f53160439be55ace7913a8c8084adf319836129
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-06-01 10:22:38 +0800

    feat: Update package dependencies and add new UI components for improved user experience

---
+++ src/app/profile/[username]/page.tsx
@@ -0,0 +1,45 @@
+import { notFound } from "next/navigation";
+
+import ProfilePageClient from "./ProfilePageClient";
+
+import {
+  getProfileByUsername,
+  getUserLikedPosts,
+  getUserPosts,
+  isFollowing,
+} from "@/actions/profile.action";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const user = await getProfileByUsername(params.username);
+
+  return {
+    title: `${user?.name ?? user?.username} `,
+    description: user?.bio || `Checkout ${user?.username}'s profile`,
+  };
+}
+
+const Page = async ({ params }: { params: { username: string } }) => {
+  const user = await getProfileByUsername(params.username);
+  if (!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
+
+  return (
+    <ProfilePageClient
+      user={user}
+      posts={posts}
+      likedPosts={likedPosts}
+      isFollowing={isCurrentUserFollowing}
+    />
+  );
+};
+
+export default Page;
