ff4c01b5eddb45efaf061aa810a822368f9652db
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-06-12 11:03:29 +0800

    refactor: Simplify NotificationIcon component by removing unused state and effect hooks

--- src/components/NotificationIcon.tsx
+++ src/components/NotificationIcon.tsx
@@ -1,6 +1,4 @@
-"use client";
 import Link from "next/link";
-import { useState, useEffect } from "react";
 import { BellIcon } from "lucide-react";
 
 import { Button } from "./ui/button";
@@ -8,33 +6,37 @@ import { Button } from "./ui/button";
 import { getNotifications } from "@/actions/notification.action";
 import { Notification } from "@/app/notifications/page";
 
-const NotificationIcon = () => {
-  const [unreadNotifsCounter, setUnreadNotifsCounter] = useState(0);
+const NotificationIcon = async () => {
+  // const [unreadNotifsCounter, setUnreadNotifsCounter] = useState(0);
+  
+  const data = await getNotifications();
+  
+  const unreadNotifications = data.filter((n) => !n.read);
 
-  const fetchNotifications = async () => {
-    try {
-      const data = await getNotifications();
+  // const fetchNotifications = async () => {
+  //   try {
+  //     const data = await getNotifications();
 
-      const unreadCount = data.filter((n) => !n.read).length;
-      setUnreadNotifsCounter(unreadCount);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  //     const unreadCount = data.filter((n) => !n.read).length;
+  //     setUnreadNotifsCounter(unreadCount);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  useEffect(() => {
-    fetchNotifications();
-    const interval = setInterval(fetchNotifications, 10000);
-    return () => clearInterval(interval);
-  }, []);
+  // useEffect(() => {
+  //   fetchNotifications();
+  //   const interval = setInterval(fetchNotifications, 10000);
+  //   return () => clearInterval(interval);
+  // }, []);
 
   return (
     <Button variant="ghost" className="flex items-center justify-start gap-2" asChild>
       <Link href="/notifications" className="relative">
         <BellIcon className="w-4 h-4" />
-        {unreadNotifsCounter > 0 && (
+        {unreadNotifications.length > 0 && (
           <span className="absolute top-0 left-[17px] flex items-center justify-center h-4 w-4 text-[9px] font-bold leading-none text-white bg-red-500 rounded-full">
-            {unreadNotifsCounter}
+            {unreadNotifications.length === 0 ? "" : unreadNotifications.length}
           </span>
         )}
         <span className="hidden lg:inline">Notifications</span>
