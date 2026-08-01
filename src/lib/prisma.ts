f81db03a53241b26a56b94790fd75cdddc9b0db5
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-08-01 18:15:06 +0800

    perf: upgrade prisma 6 to 7

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,18 @@
-import { PrismaClient, Prisma } from "@/generated/prisma";
+import { PrismaClient, Prisma } from "@/generated/prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+const connectionString = process.env.DATABASE_URL!;
+
+const adapter = new PrismaNeon({
+  connectionString,
+});
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
